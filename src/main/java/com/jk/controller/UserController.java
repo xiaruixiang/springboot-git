8d59d3b3e55c9d941c3e7040a46801ee6c7cafd1
xiaruixiang <1162894028@qq.com>
2020-08-19 14:55:24 +0800

    删除无用引用

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -2,7 +2,7 @@ package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * @program: springboot-git
