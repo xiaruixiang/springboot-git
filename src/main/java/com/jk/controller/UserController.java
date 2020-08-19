8314167651d7560defee304e75a8c931e32cecaa
xiaruixiang <1162894028@qq.com>
2020-08-19 11:47:29 +0800

    添加controller

---
+++ src/main/java/com/jk/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @program: springboot-git
+ * @author: xrx
+ * @created: 2020/08/19 11:46
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
